ee66b2159e288a70f4fe3f71c50b608d94162378
Michael G. Bryan III <mikeybryan2310@gmail.com>
2025-06-12 21:09:27 -0500

    first commit - with all files

---
+++ src/test/java/com/farrin/farrin/FarrinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farrin.farrin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarrinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
